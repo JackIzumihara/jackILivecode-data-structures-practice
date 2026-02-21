eba0ee89f35a7b6820546851322dc2f35773a28f
JackIzumihara <1551500700@k12.hi.us>
2026-02-21 11:15:13 -1000

    Update arrays.js

---
+++ arrays.js
@@ -0,0 +1,13 @@
+/*let types = [];
+let pokemon = [];
+
+pokemon[0] = "Charmander";
+pokemon[1] = "Froakie";
+pokemon[2] = "Regice";
+
+types[0] = "Fire";
+types[1] = "Water";
+types[2] = "Ice";
+console.log(pokemon);
+
+console.log(types[1]);*/
